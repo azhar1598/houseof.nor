878abddf65a9fe6c14161d29253452f7c081a6e2
azhar1598 <mohammedazhar.1598@gmail.com>
2026-06-26 23:26:35 +0530

    Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Jost, Vazirmatn } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { LanguageProvider } from "./i18n";
+
+const GA_MEASUREMENT_ID = "G-H94NBKMH9R";
+const CLARITY_PROJECT_ID = "xd6qn3wbqp";
 
 const cormorant = Cormorant_Garamond({
   variable: "--font-cormorant",
@@ -54,6 +58,27 @@ export default function RootLayout({
     >
       <body className="grain min-h-full flex flex-col bg-background text-foreground">
         <LanguageProvider>{children}</LanguageProvider>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
+          `}
+        </Script>
       </body>
     </html>
   );
